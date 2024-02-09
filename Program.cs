1c11c4e9da58b0a523ed97f429d104cbc7343868
Azerty <Azerty@DESKTOP-HHU9R9S>
2024-02-09 14:29:11 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorComplex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
