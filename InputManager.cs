957b56bbcc10eabdebd37967091e124730db027a
Neitan_Mad <mad.nataniel@gmail.com>
2024-02-17 19:02:49 +0300

    Done.

---
+++ InputManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorComplex
+{
+    public class InputManager
+    {
+        public static ComplexNumber ReadComplexNumber()
+        {
+            Console.Write("Введите вещественную часть: ");
+            double real = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите мнимую часть: ");
+            double imaginary = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+
+            return new ComplexNumber { Real = real, Imaginary = imaginary };
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using CalculatorComplex.Operation;
 using Serilog;
-using Serilog.Sinks.File;
 
 namespace CalculatorComplex
 {
@@ -14,37 +13,56 @@ namespace CalculatorComplex
                 .CreateLogger();
 
             Log.Information("Application started");
+            Console.WriteLine();
+
+            bool shouldExit = false;
 
             // Создание экземпляра калькулятора с требуемыми операциями
             ComplexCalculator calculator = new ComplexCalculator(
-                new AdditionOperation(),
-                new MultiplicationOperation(),
-                new DivisionOperation()
-                );
+            new AdditionOperation(),
+            new MultiplicationOperation(),
+            new DivisionOperation()
+            );
 
-            // Создание комплексных чисел
-            ComplexNumber num1 = new ComplexNumber { Real = 2, Imaginary = 3 };
-            ComplexNumber num2 = new ComplexNumber { Real = 4, Imaginary = 5 };
+            while (!shouldExit)
+            {
+                // Создание комплексных чисел
+                Console.WriteLine("Введите первое комплексное число: ");
+                ComplexNumber num1 = InputManager.ReadComplexNumber();
 
-            // Сложение
-            Log.Information("Addition");
-            ComplexNumber sum = calculator.Add(num1, num2);
-            Console.WriteLine($"Sum: {sum.Real} + {sum.Imaginary}i");
+                Console.WriteLine("Введите второе комплексное число: ");
+                ComplexNumber num2 = InputManager.ReadComplexNumber();
 
-            // Умножение
-            Log.Information("Multiplication");
-            ComplexNumber product = calculator.Multiply(num1, num2);
-            Console.WriteLine($"Product: {product.Real} + {product.Imaginary}i");
+                // Сложение
+                Log.Information("Addition");
+                ComplexNumber sum = calculator.Add(num1, num2);
+                Console.WriteLine($"Sum: {sum.Real} + {sum.Imaginary}i");
+                Console.WriteLine();
 
-            // Деление
-            Log.Information("Division");
-            ComplexNumber quotient = calculator.Divide(num1, num2);
-            Console.WriteLine($"Quotient: {quotient.Real} + {quotient.Imaginary}i");
+                // Умножение
+                Log.Information("Multiplication");
+                ComplexNumber product = calculator.Multiply(num1, num2);
+                Console.WriteLine($"Product: {product.Real} + {product.Imaginary}i");
+                Console.WriteLine();
+
+                // Деление
+                Log.Information("Division");
+                ComplexNumber quotient = calculator.Divide(num1, num2);
+                Console.WriteLine($"Quotient: {quotient.Real} + {quotient.Imaginary}i");
+                Console.WriteLine();
+
+                Console.WriteLine("Введите exit, чтобы выйти, или нажмите Enter, чтобы продолжить.");
+
+                string input = Console.ReadLine();
+                if (input.ToLower() == "exit")
+                {
+                    shouldExit = true;
+                    Console.WriteLine();
+                }
+            }
 
             Log.Information("Application finished");
             Log.CloseAndFlush();
-
-            Console.ReadKey();
         }
     }
 }
